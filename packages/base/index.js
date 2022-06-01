87da9cf5ea8b87566c20e9b5db47544d74d98a9b
André Felipe Schulle <33896558+andrefelipeschulle@users.noreply.github.com>
2022-06-01 09:19:07 -0300

    Update index.js

--- packages/base/index.js
+++ packages/base/index.js
@@ -75,7 +75,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       typescript: {
-        project: ['./*/**/tsconfig.json'],
+        project: ['./*/**/tsconfig.json', './tsconfig.json'],
       },
     },
   },
