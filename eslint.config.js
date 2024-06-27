a215c708800ed477a4363beaa7e4d50ec1e1cfcf
André Felipe Schulle <andrefelipeschulle@icloud.com>
2024-06-27 17:12:07 -0300

    fix(next-config): update next-plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
 import { presets } from './dist/index.js';
 
-export default [...presets.base, ...presets.typescript,...presets.react, ...presets.next];
+export default [...presets.base, ...presets.typescript];
